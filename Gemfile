e98295886618807390d0fc7b6b0c2e2b9b7ea3f9
Conroy Whitney <conroy.whitney@gmail.com>
2013-10-18 05:38:34 +0000

    added devise to dummy app per RailsApps tutorial http://railsapps.github.io/tutorial-rails-devise-rspec-cucumber.html

--- Gemfile
+++ Gemfile
@@ -10,5 +10,7 @@ gemspec
 # Git. Remember to move these dependencies to your gemspec before releasing
 # your gem to rubygems.org.
 
+gem "devise"
+
 # To use debugger
 # gem 'debugger'
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  # From "Devise with RSpec and Cucumber" Rails Tutorial
+  # http://railsapps.github.io/tutorial-rails-devise-rspec-cucumber.html
+#  authenticated :user do
+#    root :to => 'home#index'
+#  end
+  root :to => "home#index"
+  devise_for :users
+  
+  # Mount Gringotts engine wherever the hell you want to
   mount Gringotts::Engine => "/authentication"
   
 end
