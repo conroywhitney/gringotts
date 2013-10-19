295fa3cb2a87a0f442da62db2e8d6422c7d491f2
Conroy Whitney <conroy.whitney@gmail.com>
2013-10-19 18:30:42 +0000

    allow prefix of "bad_" on factories to have .valid?==false

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
-     it 'is valid' do
-       FactoryGirl.build(factory_name).should be_valid
-     end
+    if factory_name.to_s.start_with? "bad_" then
+      it 'is NOT valid' do
+        FactoryGirl.build(factory_name).should_not be_valid
+      end
+    else
+      it 'is valid' do
+        FactoryGirl.build(factory_name).should be_valid
+      end
+    end
   end
 end
