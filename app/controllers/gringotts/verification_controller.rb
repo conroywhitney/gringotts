4a3767387ce7ca392bd7175770631db86143c0ca
Conroy Whitney <conroy.whitney@gmail.com>
2013-10-20 16:25:23 +0000

    verification controller/route for challenge

---
+++ app/controllers/gringotts/verification_controller.rb
@@ -0,0 +1,8 @@
+require_dependency "gringotts/application_controller"
+
+module Gringotts
+  class VerificationController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Gringotts::Engine.routes.draw do
+
   root :to => "settings#index"
+  
   get "settings", :to => "settings#index", :as => :settings_url
   post "settings/update", :to => "settings#update", :as => :update_settings_url
+  
+  get "verify", :to => "verification#index", :as => :verification_url
+  
 end
