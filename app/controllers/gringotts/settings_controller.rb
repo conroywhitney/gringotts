d1db1b08af098a158a242e0c6810e6572956edd7
Conroy Whitney <conroy.whitney@gmail.com>
2013-10-20 16:24:52 +0000

    settings controller -- load user settings & redirect on success

--- app/controllers/gringotts/settings_controller.rb
+++ app/controllers/gringotts/settings_controller.rb
@@ -3,24 +3,27 @@ require_dependency "gringotts/application_controller"
 module Gringotts
   class SettingsController < ApplicationController
     
-    def index
-      # if this is the first time the user has been to this settings page, create them
-      # otherwise, look them up from last time they visited
-      # this is lazy-creation of Gringotts users, so we don't have to tie in with normal user creation
-      @user = Gringotts::User.find_or_create_by(user_id: current_user.id)
+    before_filter :load_gringotts_user
+    
+    def index  
     end
     
     def update
-      @user = User.new(user_params)
+      @user.assign_attributes(user_params)
       
-      if @user.valid?
-        redirect_to challenge_url
+      if @user.save
+        redirect_to root_url
       else
         render :index
       end
     end
     
 private
+    
+    def load_gringotts_user
+      # this is lazy-creation of Gringotts users, so we don't have to tie in with normal user creation
+      @user = Gringotts::User.find_or_initialize_by(user_id: current_user.id)
+    end
     
     def user_params
       params.require(:user).permit(:active, :phone_number)
