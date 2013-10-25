e423f9173f3d5cff6a682e08d4c4b0ac13276fe9
Conroy Whitney <conroy.whitney@gmail.com>
2013-10-25 16:39:23 +0000

    vault factory avoids duplication validation errors when re-used across associations solution found on StackOverflow: http://bit.ly/1d7itAw

--- spec/factories/gringotts_vaults.rb
+++ spec/factories/gringotts_vaults.rb
@@ -6,6 +6,11 @@ FactoryGirl.define do
     
     factory :good_gringotts_vault do
       user_id FactoryGirl.create(:user).id
+      
+      # So we can re-use this factory across multiple associations
+      # without receiving a duplicate validation error
+      # Thanks to: http://stackoverflow.com/questions/7145256/find-or-create-record-through-factory-girl-association
+      initialize_with { Gringotts::Vault.where(user_id: user_id).first_or_create }
     end
     
     factory :bad_missing_user_gringotts_vault do
