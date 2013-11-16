06b8b6898d1b5719c648ae7cdb2590f3e160afc8
Conroy Whitney <conroy.whitney@gmail.com>
2013-11-16 02:35:18 +0000

    backwards compatibility -- removing index before renaming table

--- db/migrate/20131020163040_rename_gringotts_users_to_gringotts_settings.rb
+++ db/migrate/20131020163040_rename_gringotts_users_to_gringotts_settings.rb
@@ -1,6 +1,6 @@
 class RenameGringottsUsersToGringottsSettings < ActiveRecord::Migration
   def change
-    remove_index :gringotts_settings, :user_id
+    remove_index :gringotts_users, :user_id
     rename_table :gringotts_users, :gringotts_settings
     add_index    :gringotts_settings, :user_id, :unique => true
   end
