6cfd23abbe6748d91736a41187fc843183e714b1
Conroy Whitney <conroy.whitney@gmail.com>
2013-11-16 02:19:18 +0000

    backwards compatibility -- rails 3 requires manual renaming index

--- db/migrate/20131020163040_rename_gringotts_users_to_gringotts_settings.rb
+++ db/migrate/20131020163040_rename_gringotts_users_to_gringotts_settings.rb
@@ -1,5 +1,7 @@
 class RenameGringottsUsersToGringottsSettings < ActiveRecord::Migration
   def change
+    remove_index :gringotts_settings, :user_id
     rename_table :gringotts_users, :gringotts_settings
+    add_index    :gringotts_settings, :user_id, :unique => true
   end
 end
